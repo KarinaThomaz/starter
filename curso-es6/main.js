cc9ce5676fbaebdf9956133c614e7699ae6ebfa4
KarinaThomaz <karinaapthomaz@gmail.com>
2020-07-29 12:18:38 -0300

    curso es6 conceitos I

---
+++ curso-es6/main.js
@@ -0,0 +1,46 @@
+const arr = [1, 3, 4, 5, 8, 9];
+
+
+
+//metodo estatico
+
+// class Matematica{
+//     static soma(a, b){
+//        return a + b;
+//     }
+// }
+
+// console.log(Matematica.soma(1, 2));
+
+// Heranca
+
+// class List {
+//     constructor(){
+//         this.data = [];
+//     }
+
+//     add(data){
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+
+// class TodoList extends List {
+//     constructor(){
+//         super();
+
+//         this.usuario = "Karina";
+//     }
+
+//     mostraUsuario(){
+//         console.log(this.usuario);
+//     }
+// }
+
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novoTodo').onclick = function(){
+//     MinhaLista.add('Novo todo');
+// }
+
+// MinhaLista.mostraUsuario();
